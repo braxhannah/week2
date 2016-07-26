1dfefb5efe9da794ac1949eb52ace03ac65e4c6f
Braxton Hannah <braxhannah@gmail.com>
2016-07-26 09:06:41 -0400

    Day one week two

---
+++ day-one.js
@@ -0,0 +1,28 @@
+// 1. Assign the message "Hello, World!" to a variable.
+
+const h="Hello, World!"
+
+// 2. Assign a different string to a different variable.
+let v="Wow This Is Cool"
+
+// 3. Assign a number to a variable.
+
+let n=10;
+
+// 4. Use string concatenation to display the number from #3 in a string.
+let str="the number from question #3 is: "
+console.log(str+n);
+
+// 5. Make an array of at least four of your favorite movies or books or bands.
+
+
+let bands=[skrillex,Mayday Parade,Green Day,A Day To Remember]
+
+// 6. Make a object of information about yourself with at least four properties.
+
+let car={
+  make:"Jeep",
+  model:"Wrangler Unlimited",
+  year:"2015",
+  color:"Red"
+}
